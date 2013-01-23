cf72a8a31cbf0aaf60f9cb0e2d14c3f46d136e83
Melissa Xie <mlxie8289@gmail.com>
2013-01-22 22:21:02 -0500

    Adjust user partial to be a row of table data

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,12 +4,7 @@ FactoryGirl.define do
     "user#{n}@example.com"
   end
 
-  sequence :name do |n|
-    "Name #{n}"
-  end
-
   factory :user do
-    name 'Some Name'
     email
     password "password"
   end
--- spec/features/user_views_users_spec.rb
+++ spec/features/user_views_users_spec.rb
@@ -3,16 +3,12 @@ require 'spec_helper'
 feature 'User views users page' do
   scenario 'and can see a list of all users' do
     signed_in_user
-    generate_users
+    create_list(:user, 5)
 
     click_link 'People'
 
     User.all.each do |user|
-      page.should have_selector('li', text: user.display_name)
+      page.should have_selector('td', text: user.display_name)
     end
-  end
-
-  def generate_users
-    5.times { create(:user) }
   end
 end
