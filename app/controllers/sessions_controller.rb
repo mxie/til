5f4f5a034b564169d74f5461d8857613a502323b
Melissa Xie <mlxie8289@gmail.com>
2013-01-25 14:01:47 -0500

    Redirect to root after failed sign-in, reposition flashes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < Clearance::SessionsController
+  def create
+    user = authenticate(params)
+
+    if user.nil?
+      flash_failure_after_create
+      render 'homes/index', status: :unauthorized
+    else
+      sign_in user
+      redirect_back_or url_after_create
+    end
+  end
+
   private
 
   def url_after_destroy
