75fffa3d8883beec77e73b9971a41c3a382bccee
Magnus Gyllensward <magnus@thoughtbot.com>
2013-03-01 14:23:36 -0500

    fixing the settings page and general layout and style

--- spec/features/user_changes_information_spec.rb
+++ spec/features/user_changes_information_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 feature 'User changes information' do
   scenario 'and it should be reflected right after submission' do
-    user = signed_in_user
-    new_name = 'First Last'
+    user = create(:user, name: 'Original Name')
+    sign_in_with(user.email, user.password)
 
     visit edit_user_path(user)
 
-    fill_in I18n.t('settings.name'), with: new_name
+    fill_in I18n.t('settings.name'), with: 'New Name'
     click_button I18n.t('settings.submit')
     user.reload
 
-    page.should have_content(new_name)
+    page.should have_content('New Name')
   end
 end
